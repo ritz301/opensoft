9082595794abdd9223667e83b901e9c42956b355
ritz301 <ritvik.raj.satya@gmail.com>
2015-12-27 13:31:52 +0530

    crawler added

---
+++ Desktop/crawler.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+import lxml.html
+
+filename = 'output.txt'
+
+def getdata():
+	v = lxml.html.document_fromstring(requests.get("http://cse.iitkgp.ac.in/faculty4.php").content)
+	# print lxml.html.tostring(v)
+	f = open(filename,'w')
+	profs = v.xpath('//font[1]/b/a/b/text()')
+	profs_details = v.xpath('//font[2]/b[1]/text()')
+	i = 1
+	for prof, prof_details in zip(profs , profs_details):
+		f.write(str(i) + '. ' + prof + ' : '+ prof_details +' \n')
+		i = i + 1
+	f.close()
+		
+if __name__=="__main__":
+	getdata()
